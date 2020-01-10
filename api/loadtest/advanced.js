07c102038839138ea2d3e4534d937646ccbb02fa
Shawn Lim <shawn.lim@local>
2020-01-10 13:51:13 +0800

    Add check for race conditions on advanced case

--- api/loadtest/advanced.js
+++ api/loadtest/advanced.js
@@ -7,11 +7,24 @@ export function loadtestAdvanced() {
   const managedCare = Math.random() * 0.25;
   const privateInsurance = Math.random() * 0.25;
   const selfPay = 1 - medicard - managedCare - privateInsurance;
-  let res = http.get(
+  http.get(
     `http://localhost:8080/calculate/advanced?medicard=${medicard}&managedCare=${managedCare}&privateInsurance=${privateInsurance}&selfPay=${selfPay}`
   );
-  // TODO: need to check whether correct response is given based on the input
-  // there must be no race conditions
+
+  // this acts as a control to see whether there is thread safety
+  const res = http.get(
+    "http://localhost:8080/calculate/advanced?medicard=0.1&managedCare=0.1&privateInsurance=0.2&selfPay=0.6"
+  );
+  check(res, {
+    "is correct response": r => {
+      if (r.status === 200) {
+        return parseInt(r.body) === 90156;
+      } else {
+        // ignoring non-200 checks as we just want to know whether response is correct for successful requests
+        return true;
+      }
+    }
+  });
 }
 
 export default function() {
